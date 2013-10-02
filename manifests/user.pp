d17a249436f71a41375e8570c799f997a85977cc
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2013-10-02 10:32:04 +0200

    Initial 0.1.0 release of puppet-module-observium
    Module includes:
     - Installation from Observium SVN repo
     - Apache vhost configuration
     - Management of users and devices

---
+++ manifests/user.pp
@@ -0,0 +1,16 @@
+define observium::user(
+  $password,
+  $level
+) {
+  $username = $name
+
+  $check_query = "SELECT user_id FROM users WHERE username = \'${username}\'"
+
+  exec { "user-${username}":
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    command => "php adduser.php ${username} ${password} ${level}",
+    cwd     => $observium::base_path,
+    onlyif  => "test -z `mysql -h ${observium::mysql_host} -u ${observium::mysql_user} -p${observium::mysql_password} -s -e \"${check_query}\" ${observium::mysql_db}`",
+  }
+
+}
